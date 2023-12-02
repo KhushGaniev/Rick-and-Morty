478bb31f06506f4ffc487eb60cbd44cfe1959181
Khush <ganievkhushbaht@gmail.com>
2023-12-02 17:11:41 +0300

    fead: set up the project

---
+++ src/views/StartView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="start">
+    This is the start page.
+    <router-link to="/products"><span>Click to continue</span></router-link>
+  </div>
+</template>
+<style lang="scss" scoped>
+span {
+  font-size: 20px;
+  font-weight: 600;
+}
+</style>
---
+++ src/views/productsPage/MyProducts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est enim debitis
+    eveniet quos. Illo facilis reprehenderit ducimus error, dolores ipsum
+    tempora incidunt pariatur commodi ratione soluta alias impedit excepturi
+    nobis?
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyProducts",
+};
+</script>
