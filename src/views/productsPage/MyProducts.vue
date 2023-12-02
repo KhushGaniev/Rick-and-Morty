3582ece684a76e14c84edd285b30a9b484ce190f
Khush <ganievkhushbaht@gmail.com>
2023-12-02 18:02:06 +0300

    feat: add cards for main page

--- src/views/productsPage/MyProducts.vue
+++ src/views/productsPage/MyProducts.vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="home">
-    <cards-person />
+  <div class="product">
+    <default-header />
+    <div class="container">
+      <div class="product__wrapper">
+        <cards-person />
+        <cards-person />
+        <cards-person />
+        <cards-person />
+        <cards-person />
+        <cards-person />
+        <cards-person />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import CardsPerson from "@/components/cards/CardsPerson.vue";
+import DefaultHeader from "@/components/DefaultHeader.vue";
 </script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1500px;
+  margin: 0 auto;
+}
+
+.product {
+  background-color: #f8f8f8;
+  height: 100vh;
+  &__wrapper {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 16px;
+    padding: 16px;
+  }
+}
+</style>
