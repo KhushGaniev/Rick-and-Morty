015aa598743c101090cd1d7a52d2fe12f39ca8ba
Khush <ganievkhushbaht@gmail.com>
2023-12-07 02:24:06 +0300

    fix: theme

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,4 @@
   <router-view />
 </template>
 
-<style lang="scss" src="@/assets/scss/normalize.scss">
-@import "@/styles/theme.scss";
-</style>
+<style lang="scss" src="@/styles/normalize.scss"></style>
